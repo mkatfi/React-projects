0de35fd7ce73e9e5b02816a49247e0269426294b
Marouane Katfi <mkatfi@c3r10p8.1337.ma>
2025-04-11 23:14:02 +0100

    (0.6)

--- cv-Application/src/App.jsx
+++ cv-Application/src/App.jsx
@@ -297,7 +297,7 @@ function App() {
   return (
     <div className="app-container">
       <div className="header">
-        <img src="./src/image/logo.png" alt="" />
+      <img src="/logo.png" alt="Logo" />
         <div className="download-section">
             <DownloadPDFButton />
             <button 
